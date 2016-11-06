5daee4d0d38bd760b5364b18532ce721b9ad4095
Christopher Dedes <christopherdedes2906@gmail.com>
2016-11-06 21:41:22 +1100

    Added some responsiveness to Services -- Work to be done further

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
-  $("#test").click(function(){
-    console.log("falg");
-    $(this).animate({
-      left: "-=500px"
-    }, 500);
-  });
-});
+/// HOVER FUNCTION FOR PORTFOLIO
+// var movePort = false;
+// $(document).ready(function(){
+//   $(".portPara").attr("style", "display: none");
+//     $("#test").click(function(){
+//       if(movePort) {
+//         $(this).animate({
+//           marginLeft: "-=500px"
+//         }, 500);
+//       });
+//       }
+//
+//   else {
+//     $("#test").click(function(){
+//       $(this).animate({marginLeft: "-=500px"}, 500);
+//       movePort = true;
+//     }
+//   });
+// });
