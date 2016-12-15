10cef2fa38c6d2c47b7e09fc984245f78a78333c
Christopher Dedes <christopherdedes2906@gmail.com>
2016-12-15 21:00:46 +1100

    Added Fade-ins for important elements - Yet to be completed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -25,3 +25,22 @@
 // 		$(".langTitle").attr("style", "visibility: hidden");
 // 	});
 // });
+
+// SCROLL FUNCTIONALITY SECTION
+
+//Scroll Variables
+var dura = {
+		duration: 1000
+}; // Duration of 1000
+
+// Scroll Functions
+window.sr = ScrollReveal();
+sr.reveal('.intro-statement', dura); // Starting statement of each section
+sr.reveal('.test1', dura); // Skills - 1,2
+sr.reveal('.test2', dura); // Skills - 3,4
+sr.reveal('.test3', dura); // Skills - 5,6
+sr.reveal('.langTitle', dura); // Language Name
+sr.reveal('.langBar', { duration: 1500 }); // Progress Bar
+sr.reveal('.langPer', { duration: 2000 }); // Progress Percentage
+sr.reveal('.abilContainer', { duration: 1500 });
+sr.reveal('.button-centering', { rotate: { x: 0, y: -100, z: 0 } }, { duration: 30000 }); // More Button
