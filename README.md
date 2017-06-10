f859a237df957eef9182afd81974890c997d3468
Khrysstopha <christopherdedes2906@gmail.com>
2017-06-11 09:38:16 +1000

    Goodmorning github!
    Editted README.md
    fixed angular issue

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# kryss-designs
-Kryss' Designs is a personal website project that I have embarked on.
+# Supr.
+Supr is a personal website project that I have embarked on.
 The goal was to understand what I knew, and what I needed to know.
 
 Languages learnt:
 HTML - 95%
 CSS - 90%
 jQuery - 75%
-SASS - 70%
+SASS - 50%
 JS - 35-40%
 AngularJS - 10%
-ReactJS - 1%
-
--- assets/js/MainController.js
+++ assets/js/MainController.js
@@ -1,8 +1,8 @@
 /* START OF MY APP MODULE */
-var app = angular.module('myApp', []);
+var app = angular.module('serviceData', []);
 app.controller('myCtrl', function($scope) {
-  $scope.intro = 'A plan right for you.';
-  /* SERVICE ANGULAR DATA */
+    $scope.intro = 'A plan right for you.';
+    /* SERVICE ANGULAR DATA */
   $scope.product = [
     // {
     //   name: 'Standard Package',
@@ -38,20 +38,27 @@ app.controller('myCtrl', function($scope) {
     //   item3: 'CMS',
     //   item4: 'Help with deployment and domain',
     //   item5: 'Specifics you wish to be implemented',
-    desc: 'A package that goes a little deeper, includes greater functionality with integrated shopping support.'
+    desc: 'A package that goes a little deeper, includes greater functionality with integrated shopping support.',
     }
-  ];
-  /* PORTFOLIO ANGULAR DATA */
-  $scope.portfolio = [
-    {
-      title: 'Personal Website',
-      image: 'imgs/kryssdesign.JPG',
-      para: 'Designed to advertise myself, Khryss\' Designs is a personal website to display my portfolio. It has been entirely built by me, as a meetng point for customers to understand what it is that I can achieve.',
-    },
-    {
-      title: 'Cooking Creations',
-      image: 'imgs/kryssdesign.jpg',
-      para: '',
-    }
-  ];
-});
+];
+  // /* PORTFOLIO ANGULAR DATA */
+  // $scope.portfolio = [
+  //   {
+  //     title: 'Personal Website',
+  //     image: 'imgs/kryssdesign.JPG',
+  //     para: 'Designed to advertise myself, Khryss\' Designs is a personal website to display my portfolio. It has been entirely built by me, as a meetng point for customers to understand what it is that I can achieve.',
+  //   },
+  //   {
+  //     title: 'Cooking Creations',
+  //     image: 'imgs/kryssdesign.jpg',
+  //     para: '',
+  //   }
+  // ];
+//   // Skills container
+//   $scope.skill = [
+//       {
+//         skill: 'Learning',
+//
+//       }
+//   ]
+}); // DON'T FORGET THIS!!!
